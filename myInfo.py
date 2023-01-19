9b7a3cc6e685515ebcba6733302513e7693ede06
VinMacri <114018421+VinMacri@users.noreply.github.com>
2023-01-19 12:23:07 -0500

    my information v2
    This is version 2 of the file myInfo

--- myInfo.py
+++ myInfo.py
@@ -5,3 +5,4 @@
 print("My name is Vincent Macri")
 print("My adress is 10 Monmouth Street, West Long Branch, NJ 07764")
 print("My phone number is 123-456-7890")
+print("My major is computer science")
